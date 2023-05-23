64abb97e97ddb5ac2711db2fb4cd6b70b3e47e71
eslionekim <kimdmstj0510@naver.com>
2023-05-23 20:21:29 +0900

    todo 모델 만들고 데이터 출력하기

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class TodoList(models.Model):
-	todos = models.CharField(max_length=30)
-    description = models.TextField()
-    important = models.BooleanField(default=False)
+	todo = models.CharField(max_length=30)
+	description = models.TextField()
+	important = models.BooleanField(default=False)
 	complete = models.BooleanField(default=False)
+ 
+def __str__(self):
+	return f'{self.todo} [{self.id}]'
